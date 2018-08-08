609ceee58348ce77bdc010df59772289e1417faf
Negresko <viniciuspeixoto2008@hotmail.com>
2018-08-09 03:50:23 +0900

    Alinhando Botões, configurando o CSS e Programando o JavaScript

--- www/components/calcular.js
+++ www/components/calcular.js
@@ -4,5 +4,26 @@ $(document).on("click","#somar", function(){
     
     var result = valor1 + valor2;
     $("#result").val(result);
+});
+$(document).on("click","#subtracao", function(){
+    var valor1 = parseInt($("#valor1").val());
+    var valor2 = parseInt($("#valor2").val());
+    
+    var result = valor1 - valor2;
+    $("#result").val(result);
+}); 
+$(document).on("click","#multiplicacao", function(){
+    var valor1 = parseInt($("#valor1").val());
+    var valor2 = parseInt($("#valor2").val());
+    
+    var result = valor1 * valor2;
+    $("#result").val(result);
+}); 
+$(document).on("click","#divisao", function(){
+    var valor1 = parseInt($("#valor1").val());
+    var valor2 = parseInt($("#valor2").val());
+    
+    var result = valor1 / valor2;
+    $("#result").val(result);
 }); 
 
