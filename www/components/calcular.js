e6636f11249c28d3cd3ec81e194f7d7ea5b0f404
Negresko <viniciuspeixoto2008@hotmail.com>
2018-08-09 03:15:36 +0900

    Inserindo JavaScript e Alterando Index.html

---
+++ www/components/calcular.js
@@ -0,0 +1,8 @@
+$(document).on("click","#somar", function(){
+    var valor1 = parseInt($("#valor1").val());
+    var valor2 = parseInt($("#valor2").val());
+    
+    var result = valor1 + valor2;
+    $("#result").val(result);
+}); 
+
